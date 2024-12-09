64cec6f3fbd8d1118ad994fb4575b074d7b90327
Ruziev Asilbek <roziyev106@gmail.com>
2024-12-09 18:40:20 +0500

    Site-header-theme-toggler click

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-.
+const elSiteHeaderThemeToggler = document.querySelector('.site-header-theme-toggler');
+
+elSiteHeaderThemeToggler.addEventListener('click', function () {
+  document.body.classList.toggle('dark-mode');
+});
